33faffd70a8a059b65f120be06f53486f20d5e24
Pascal Poizat <pascal.poizat@gmail.com>
2024-06-17 09:53:30 +0200

    enhance error treatment

--- src/main/java/com/cifre/sap/su/GoblinReportMojo.java
+++ src/main/java/com/cifre/sap/su/GoblinReportMojo.java
@@ -5,13 +5,20 @@ package com.cifre.sap.su;
 
 import java.util.Locale;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Optional;
+
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.util.Optional;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.maven.artifact.Artifact;
@@ -25,10 +32,17 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.reporting.AbstractMavenReport;
 import org.apache.maven.reporting.MavenReportException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Mojo(name = "goblin", defaultPhase = LifecyclePhase.SITE, requiresDependencyResolution = ResolutionScope.RUNTIME, requiresProject = true, threadSafe = true)
 public class GoblinReportMojo extends AbstractMavenReport {
     @Parameter(property = "apiUrl", defaultValue = "http://localhost:8080")
     private String apiUrl;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final String releaseRoute = "/release";
 
     @Override
     public String getOutputName() {
@@ -48,11 +62,12 @@ public class GoblinReportMojo extends AbstractMavenReport {
     @Override
     protected void executeReport(Locale locale) throws MavenReportException {
         MavenProject project = getProject();
-        final List<String> FIELDS = List.of("CVE", "FRESHNESS");
-        final List<String> COLUMNS = List.of("Artifact", "Version", "Scope", "CVE", "FRESHNESS");
+        final List<String> FIELDS = List.of("cve_aggregated", "freshness");
+        final List<String> COLUMNS = List.of("Artifact", "Version", "Scope", "Aggregated CVE",
+                "Freshness (missed releases)");
 
         Set<Artifact> artifacts = project.getDependencyArtifacts();
-        Map<Artifact, Map<String, String>> artifactMap = retrieveInformation(FIELDS, artifacts);
+        Map<Artifact, Optional<Map<String, Optional<String>>>> artifactMap = retrieveInformation(artifacts, FIELDS);
 
         Log logger = getLog();
         logger.info("Generating " + getOutputName() + ".html"
@@ -110,7 +125,8 @@ public class GoblinReportMojo extends AbstractMavenReport {
 
             for (String field : FIELDS) {
                 mainSink.tableCell();
-                mainSink.text(artifactMap.get(artifact).get(field));
+                String value = artifactMap.get(artifact).flatMap(a -> a.get(field)).orElse("?");
+                mainSink.text(value);
                 mainSink.tableCell_();
             }
             mainSink.tableRow_();
@@ -122,6 +138,12 @@ public class GoblinReportMojo extends AbstractMavenReport {
         mainSink.body_();
     }
 
+    /**
+     * Returns the id of an artifact (i.e., a String of the form gid:aid).
+     * 
+     * @param artifact
+     * @return the id of the artifact
+     */
     private String artifactId(Artifact artifact) {
         return artifact.getGroupId() + ":" + artifact.getArtifactId();
     }
@@ -140,18 +162,72 @@ public class GoblinReportMojo extends AbstractMavenReport {
         }
     }
 
-    private Map<Artifact, Map<String, String>> retrieveInformation(List<String> fields, Set<Artifact> artifacts) {
-        Map<Artifact, Map<String, String>> artifactMap = new HashMap<>();
-        final String url = apiUrl + "/release";
-        for (Artifact artifact : artifacts) {
-            String artifactId = artifactId(artifact);
-            Map<String, String> artifactInformation = new HashMap<>(fields.size());
-            for (String field : fields) {
-                artifactInformation.put(field, "xxx");
+    private Map<Artifact, Optional<Map<String, Optional<String>>>> retrieveInformation(Set<Artifact> artifacts,
+            List<String> metrics) {
+        Function<Artifact, Optional<Map<String, Optional<String>>>> mapper = a -> getMetricsForArtifact(a, metrics);
+        return artifacts.stream().collect(Collectors.toMap(Function.identity(), mapper));
+    }
+
+    private Optional<Map<String, Optional<String>>> getMetricsForArtifact(Artifact artifact, List<String> metrics) {
+        final String url = apiUrl + releaseRoute;
+
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("groupId", artifact.getGroupId());
+        requestData.put("artifactId", artifact.getArtifactId());
+        requestData.put("version", artifact.getVersion());
+        requestData.put("addedValues", metrics);
+
+        Optional<Map<String, Optional<String>>> rtr;
+
+        try {
+            String requestBody = objectMapper.writeValueAsString(requestData);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200 && !response.body().equals("{}")) {
+                rtr = Optional.of(extractValues(artifact, response.body(), metrics));
+            } else {
+                Log logger = getLog();
+                logger.warn("Unknown release: " + artifact);
+                rtr = Optional.empty();
             }
-            artifactMap.put(artifact, artifactInformation);
+            client.close();
+            return rtr;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
         }
-        return artifactMap;
+    }
+
+    private static final Map<String, Function<JsonNode, String>> extractors = new HashMap<>();
+    static {
+        extractors.put("cve_aggregated", String::valueOf);
+        extractors.put("freshness", n -> n.get("numberMissedRelease").asText());
+    }
+
+    private Map<String, Optional<String>> extractValues(Artifact artifact, String responseBody, List<String> metrics)
+            throws JsonProcessingException {
+        JsonNode rootNode = objectMapper.readTree(responseBody);
+        JsonNode nodes = rootNode.path("nodes").get(0);
+
+        Map<String, Optional<String>> values = new HashMap<>();
+        for (String metric : metrics) {
+            JsonNode node = nodes.path(metric);
+            if (!node.isMissingNode()) {
+                values.put(metric, Optional.ofNullable(extractors.get(metric)).map(f -> f.apply(node)));
+            } else {
+                Log logger = getLog();
+                logger.warn("Unknown metric " + metric + " for release " + artifact);
+                values.put(metric, Optional.empty());
+            }
+        }
+        return values;
     }
 
 }
